ca49bb93a79029cb3ffa65e0b057689facb6865c
Dillshad685 <dillshadb2@gmail.com>
2025-12-01 01:21:32 +0530

    Initial commit

---
+++ common.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+echo "$0"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1) #$0 refers to the file which is running currently in
+#the server which is mongodb.sh removes .sh and adds .log
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"  #/var/log/shell-roboshop/mongodb.log
+
+mkdir -p $LOGS_FOLDER
+echo "$LOG_FILE"
+echo "script execution start time: $(date)" | tee -a $LOG_FILE    #appends the output to the logfile
+
+USERID=$(id -u)    
+
+#returns pwd user id which should be 0 if it is sudo user and that 0 value is stoed in userid
+
+Checks_sudo_user(){
+    if [ $USERID -ne 0 ]; then
+        echo -e "$R Please run in sudo user $N" | tee -a $LOG_FILE
+        exit 1
+    #failure code is other than "0"
+    fi
+}
+
+VALIDATE(){ #function receives input as args
+  if [ $1 -ne 0 ]; then  
+     echo -e " Installation failed .. $R FAILURE $N" | tee -a $LOG_FILE
+     exit 1
+  else
+     echo -e "$2 .. $G SUCCESS $N" | tee -a $LOG_FILE
+  fi
+}
---
+++ mongodb.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source ./common.sh
+
+Checks_sudo_user()
+
+cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>>$LOG_FILE
+VALIDATE $? "Adding mongorepo"
+
+dnf install mongodb-org -y &>>$LOG_FILE
+VALIDATE $? "INSTALLING mongoDB"
+
+systemctl enable mongod &>>$LOG_FILE
+VALIDATE $? "Enabling mongoDB"
+systemctl start mongod &>>$LOG_FILE
+VALIDATE $? "starting mongoDB"
+
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>>$LOG_FILE
+VALIDATE $? "allowing all ports" #using sed we can insert in the file 
+
+systemctl restart mongod &>>$LOG_FILE
+VALIDATE $? "restarting mongoDB"
